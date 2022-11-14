fb4547a0bc7a2271372287ee1988e5da3ef9c79f
jessica <jessicahercon@gmail.com>
2022-11-14 13:41:46 -0600

    search function

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
  * Funciones CRUD para Jex y Vanne: Obtener detalle de un post (READ) y eliminar un post (DELETE)
  */
 
-const fireBaseDB = "https://todo-list-ec668-default-rtdb.firebaseio.com/";
+const fireBaseDB = "https://devto-5a011-default-rtdb.firebaseio.com/";
 
 const post = {
   title: "Cafe con pan",
@@ -60,24 +60,21 @@ const updatePost = (id, newData) => {
     });
 };
 
-const getAllPosts = () => {
+const getAllPosts = async () => {
   const posts = [];
   const url = `${fireBaseDB}articles.json`;
-  fetch(url, { method: "GET" })
-    .then((response) => response.json())
-    .then((data) => {
-      //console.log("data: ", data);
-      const keys = Object.keys(data);
-      //console.log("keys: ", keys);
-      keys.forEach((key) => {
-        let post = data[key];
-        post.id = key;
-        post.creationDate = new Date(post.creationDate)
-        posts.push(post);
-        //console.log("value", data[key]["description"]);
-      });
+  const response= await fetch(url, { method: "GET" })
+  const data= await response.json()
+    //console.log("data: ", data);
+    const keys = Object.keys(data);
+    //console.log("keys: ", keys);
+    keys.forEach((key) => {
+      let post = data[key];
+      post.id = key;
+      post.creationDate = new Date(post.creationDate)
+      posts.push(post);
+      //console.log("value", data[key]["description"]);
     });
-  console.log(posts.length)
   return posts;
 };
 
@@ -104,6 +101,51 @@ const filtrarFechas = (timePeriod, posts) => {
 
 
 //updatePost("-NGmn-xmrY2GbRnG4wQk", post1);
-let allPosts = getAllPosts();
+// let allPosts = getAllPosts();
+// console.log(allPosts);
 
-const ultimaSemana = setTimeout(filtrarFechas, 5000, 'semanas', allPosts)
+// const ultimaSemana = setTimeout(()=>filtrarFechas('semanas', allPosts), 5000)
+
+//Creating 20 posts + rating 
+
+const createFakePosts=()=>{
+  for (let index = 1; index <=20; index++) {
+    createPost({
+      title:"Post"+ index,
+     description:" Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit veniam fugiat consequatur sapiente deleniti aperiam labore provident at nobis dolorum veritatis, magnam nostrum iusto consectetur et excepturi rem nulla sed.", 
+     rating: String(Math.floor(Math.random() * 5))
+    });
+    
+  }
+}
+
+
+
+//Main function 
+
+const main =async()=>{
+  let allPosts=[];
+allPosts =await getAllPosts()
+
+//Search posts
+const searchPosts=(searchTerm)=>{
+    if (searchTerm != "") {
+      return allPosts.filter((post)=>{
+  if (post.title.includes(searchTerm)) {
+    return true
+  }
+   return false
+     })    
+    }
+  }
+
+  searchButton.addEventListener('click',()=>{
+    const searchTerm= searchInput.value;
+  console.log("search",searchPosts(searchTerm));
+    
+  });
+
+}
+
+main();
+
